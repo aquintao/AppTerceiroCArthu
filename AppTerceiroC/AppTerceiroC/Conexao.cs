71fa8b7c75b8f08aefc3b2c563a3dcffabd3b059
aluno <aluno@L03MICRO21>
2023-10-09 09:58:08 -0300

    Add project files.

---
+++ AppTerceiroC/AppTerceiroC/Conexao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTerceiroC
+{
+    public static class Conexao
+    {
+        public static string strConexao = "server=bancomodulo.ddns.net;port=3306;user=arthur_santos;password=31198;database=tbl_starte;";
+    }
+}
